6acbec951419b10ad55abbd1260db65fec8fb758
nolannmoisis <nolannmoisis@gmail.com>
2024-12-06 01:06:49 -0500

    fefesf

--- js/script.js
+++ js/script.js
@@ -1,8 +1,6 @@
-// Script pour afficher et fermer la sidebar
 const menuToggle = document.getElementById('menu-toggle');
 const sidebar = document.getElementById('sidebar');
 
-// Ouvre ou ferme la sidebar quand le hamburger est cliqué
 menuToggle.addEventListener('click', () => {
-    sidebar.classList.toggle('open');  // Ajoute ou retire la classe 'open' pour afficher ou cacher la sidebar
+    sidebar.classList.toggle('open'); 
 });
